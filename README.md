af9a0897e4382542cd01109377d305ad8520b330
Shaun Janssens <shaunjanssens@gmail.com>
2015-10-21 13:00:16 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WEBDADII-OP2
 Tutorial voor jQuery plugin: ContentTools
+
+[Project website](http://shaujans.github.io/WEBDADII-OP2/)
---
+++ js/editor.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    var editor;
+
+    editor = ContentTools.EditorApp.get();
+    editor.init('*[data-editable]', 'data-name');
+
+});
